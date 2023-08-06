c464050296dcc587d2e2367e1861aa6fa6c2b6c2
John <jddhalogamer@gmail.com>
2023-08-06 17:42:59 -0500

    built user routes, controllers, and linked to index

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import cookieParser from 'cookie-parser'
 import videoRoutes from './routes/videos.js'
 import commentRoutes from './routes/comments.js'
+import userRoutes from './routes/users.js'
 
 const app = express();
 
 app.use(cookieParser())
 app.use(express.json())
+app.use('/api/users', userRoutes)
 app.use('/api/video', videoRoutes)
 app.use('/api/comments', commentRoutes)
+
 
 app.use((err, req, res, next) => {
     const status = err.status || 500

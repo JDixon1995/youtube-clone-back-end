956745422b851c896fc1a9922ba1062b73464376
John <jddhalogamer@gmail.com>
2023-10-09 19:02:42 -0500

    updateComment function routing.

--- routes/comments.js
+++ routes/comments.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { addComment, deleteComment, getComments } from "../controllers/comment.js";
+import { addComment, deleteComment, getComments, updateComment } from "../controllers/comment.js";
 import { verifyToken } from "../verifyToken.js"
 const router = express.Router();
 
 router.post("/", verifyToken, addComment)
 router.delete("/:id", verifyToken, deleteComment)
+router.put("/:id", verifyToken, updateComment)
 router.get("/:videoId", getComments)
 
 export default router;
